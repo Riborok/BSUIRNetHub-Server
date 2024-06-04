9e79dfff6bbd4dccf5e4e45b95e6100a56a486a7
Egor Pankratiev <egor.pankratiew@gmail.com>
2024-06-04 03:02:32 +0300

    added Repository annotation

--- src/main/kotlin/com/bsuirnethub/repository/GroupRepository.kt
+++ src/main/kotlin/com/bsuirnethub/repository/GroupRepository.kt
@@ -4,7 +4,9 @@ import com.bsuirnethub.alias.GroupId
 import com.bsuirnethub.entity.GroupEntity
 import com.bsuirnethub.entity.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface GroupRepository : JpaRepository<GroupEntity, Long> {
     fun deleteByGroupIdAndUser(groupId: GroupId, user: UserEntity): Int
 }
--- src/main/kotlin/com/bsuirnethub/repository/TeacherRepository.kt
+++ src/main/kotlin/com/bsuirnethub/repository/TeacherRepository.kt
@@ -4,7 +4,9 @@ import com.bsuirnethub.alias.TeacherId
 import com.bsuirnethub.entity.TeacherEntity
 import com.bsuirnethub.entity.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface TeacherRepository : JpaRepository<TeacherEntity, Long> {
     fun deleteByTeacherIdAndUser(teacherId: TeacherId, user: UserEntity): Int
 }
